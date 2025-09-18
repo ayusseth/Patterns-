463ecff754a48390e17c27c4b814ead18f52719e
ayusseth <er.ayushseth@gmail.com>
2025-09-18 11:53:11 +0530

    new Pattern is added

---
+++ src/main/java/Hollow_reverse_triangle.java
@@ -0,0 +1,35 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Hollow_reverse_triangle
+{
+    public static void main(String[] args) {
+        try(Scanner sc =new Scanner(System.in)){
+            int n=sc.nextInt();
+            if(n<0){
+                System.out.println("Input must be greater than 0");
+                return;
+            }
+            for (int i = n ;i >=1 ; i--) {
+                System.out.print(" ".repeat(n-i));
+                for (int j = 1; j <=i ; j++) {
+                    if(j==1||j==i||i==n){
+                        System.out.print("* ");
+                    }else{
+                        System.out.print("  ");
+                    }
+                }
+                System.out.println();
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Input must be positive Integer");
+        }
+    }
+}
+//* * * * * * *
+// *         *
+//  *       *
+//   *     *
+//    *   *
+//     * *
+//      *
