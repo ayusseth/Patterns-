6448d5cf4967003c0bb718f1f1cfb293aa6a2d9f
ayusseth <er.ayushseth@gmail.com>
2025-09-18 10:06:57 +0530

    try block is added

--- src/main/java/Reverse_right_angle_triangle.java
+++ src/main/java/Reverse_right_angle_triangle.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 public class Reverse_right_angle_triangle
 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n= sc.nextInt();
-        for (int i = n; i >=1; i--) {
-            System.out.print("* ".repeat(i));
-            System.out.println();
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            for (int i = n; i >= 1; i--) {
+                System.out.print("* ".repeat(i));
+                System.out.println();
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Input must be positive Integer");
         }
     }
 }
