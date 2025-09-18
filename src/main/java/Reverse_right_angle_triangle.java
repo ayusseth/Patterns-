1bf69a3df588d21db85aee1f9235555502a1efae
ayusseth <er.ayushseth@gmail.com>
2025-09-18 10:07:14 +0530

    Exception handling

--- src/main/java/Reverse_right_angle_triangle.java
+++ src/main/java/Reverse_right_angle_triangle.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
-public class Reverse_right_angle_triangle
-{
+public class Reverse_right_angle_triangle {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             int n = sc.nextInt();
