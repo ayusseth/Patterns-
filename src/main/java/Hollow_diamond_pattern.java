041292e380742d7116993ea2b796429b90174e7b
ayusseth <er.ayushseth@gmail.com>
2025-09-18 15:50:17 +0530

    new Pattern is added

---
+++ src/main/java/Hollow_diamond_pattern.java
@@ -0,0 +1,38 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Hollow_diamond_pattern {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            if (n <= 0) {
+                System.out.println("Input must be greater than 0");
+                return;
+            }
+            for (int i = 1; i <= n; i++) {
+                System.out.print(" ".repeat(n - i));
+                for (int j = 1; j <= i; j++) {
+                    if (j == 1 || j == i) {
+                        System.out.print("* ");
+                    } else {
+                        System.out.print("  ");
+                    }
+                }
+                System.out.println();
+            }
+            for (int i = n; i >= 1; i--) {
+                System.out.print(" ".repeat(n - i));
+                for (int j = 1; j <= i; j++) {
+                    if (j == 1 || j == i) {
+                        System.out.print("* ");
+                    } else {
+                        System.out.print("  ");
+                    }
+                }
+                System.out.println();
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Input must be positive Integer");
+        }
+    }
+}
