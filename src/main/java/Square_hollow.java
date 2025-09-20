60ae37aae505609b16a970222164f9d44a813fbd
ayusseth <er.ayushseth@gmail.com>
2025-09-20 11:18:06 +0530

    space added

--- src/main/java/Square_hollow.java
+++ src/main/java/Square_hollow.java
@@ -8,7 +8,7 @@ public class Square_hollow
         try(Scanner sc=new Scanner(System.in);) {
             int m = sc.nextInt(), n=sc.nextInt();
             if (m > 0 && n > 0) {
-                System.out.println("*".repeat(n));
+                System.out.println("* ".repeat(n));
                 for (int i = 2; i <m ; i++) {
                     if(n>1){
                         System.out.println("*"+" ".repeat(n-2)+"*");
