0f91f1f072d4118e680f5072d413d58da6809371
ayusseth <er.ayushseth@gmail.com>
2025-09-20 11:18:53 +0530

    Square hollow pattern code updated

--- src/main/java/Square_hollow.java
+++ src/main/java/Square_hollow.java
@@ -5,20 +5,20 @@ public class Square_hollow
 {
     public static void main(String[] args) {
 
-        try(Scanner sc=new Scanner(System.in);) {
+        try(Scanner sc=new Scanner(System.in)) {
             int m = sc.nextInt(), n=sc.nextInt();
             if (m > 0 && n > 0) {
                 System.out.println("* ".repeat(n));
                 for (int i = 2; i <m ; i++) {
                     if(n>1){
-                        System.out.println("*"+" ".repeat(n-2)+"*");
+                        System.out.println("*"+"  ".repeat(n-2)+" *");
                     }
                     else{
                         System.out.println("*");
                     }
                 }
                 if(m>1){
-                    System.out.println("*".repeat(n));
+                    System.out.println("* ".repeat(n));
                 }
             }
             else{
